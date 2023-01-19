78f57d67a44ac14811b3c13ed04a02692d5d0c5b
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-01-19 12:32:53 -0300

    correcao de codigo

--- desafio02/main.js
+++ desafio02/main.js
@@ -30,13 +30,13 @@ function verificar() {
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Bebe
-                img.setAttribute('src', 'idoso-f.png')
+                img.setAttribute('src', 'bebe-f.png')
             } else if (idade < 21) {
                 //Jovem
-                img.setAttribute('src', 'idoso-f.png')
+                img.setAttribute('src', 'jovem-f.png')
             } else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', 'idoso-f.png')
+                img.setAttribute('src', 'adulto-f.png')
             } else {
                 //Idoso
                 img.setAttribute('src', 'idoso-f.png')
@@ -44,6 +44,7 @@ function verificar() {
         }
         //res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
