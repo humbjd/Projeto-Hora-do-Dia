637998c50c798aff5d5f096ec461062c3cfe9884
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-01-24 15:06:03 -0300

    Finalização de cógigo

--- moduloE/aula 02/main.js
+++ moduloE/aula 02/main.js
@@ -1,19 +1,32 @@
-function verificar() {
-    var ini =  document.getElementById('txtini')
-    var fim = document.getElementById('txtfim')
-    var passo = document.getElementById('txtpas')
-    var res = document.getElementById('res')
+function calcular() {
+    let ini =  document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpas')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Faltam dados!')   
     } else {
-        res.innerHTML = 'Contando'
-        let i = number(ini.value)
-        let f = number(fim.value)
-        let p = number(passo.value)
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
+
+        if (i < f){
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c -=p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }   
+        }
+         res.innerHTML += `\u{1F3C1}`    
     }
 }
