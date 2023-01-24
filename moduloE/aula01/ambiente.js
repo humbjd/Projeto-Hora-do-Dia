78b82b03ecc61adaf78c8fb70b13e3051148abe7
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-01-24 10:04:50 -0300

    Praticando JS

---
+++ moduloE/aula01/ambiente.js
@@ -0,0 +1,18 @@
+/*var c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)
+
+
+
+for ( var c = 1; c <= 6; c++) {
+    console.log(c)
+}
+*/
+
